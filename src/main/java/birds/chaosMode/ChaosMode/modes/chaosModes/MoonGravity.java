ce4836044edf3a87560442b61cb0b9fb792649bf
PepperLola <julian.orion.wright@gmail.com>
2020-01-29 22:07:02 -0800

    finished moon gravity i think

--- src/main/java/birds/chaosMode/ChaosMode/modes/chaosModes/MoonGravity.java
+++ src/main/java/birds/chaosMode/ChaosMode/modes/chaosModes/MoonGravity.java
@@ -3,11 +3,16 @@ package birds.chaosMode.ChaosMode.modes.chaosModes;
 import birds.chaosMode.ChaosMode.modes.ListenerMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class MoonGravity extends ListenerMode {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-
+        if (event.getFrom().getBlockY() != event.getTo().getBlockY()) {
+            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 10, 1));
+        }
     }
 }
