bb850d9c0347e3f5287b4fbddd774f28059edaf5
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2020-01-29 22:48:32 -0800

    Levitate only on jump, change duration of slowfalling and levitation

--- src/main/java/birds/chaosMode/ChaosMode/modes/chaosModes/MoonGravity.java
+++ src/main/java/birds/chaosMode/ChaosMode/modes/chaosModes/MoonGravity.java
@@ -1,20 +1,35 @@
 package birds.chaosMode.ChaosMode.modes.chaosModes;
 
 import birds.chaosMode.ChaosMode.modes.ListenerMode;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.potion.Potion;
+import org.bukkit.event.player.PlayerStatisticIncrementEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.Objects;
 
 public class MoonGravity extends ListenerMode {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (event.getTo().getBlockY() > event.getFrom().getBlockY()) {
-            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 1, 1));
-        } else if (event.getTo().getBlockY() < event.getFrom().getBlockY()) {
-            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 1, 1));
+        Player player = event.getPlayer();
+
+        if (Objects.requireNonNull(event.getTo()).getBlockY() < event.getFrom().getBlockY()) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 10, 1));
         }
+    }
+
+    @EventHandler
+    public void onJump(PlayerStatisticIncrementEvent event) {
+        Player player = event.getPlayer();
+
+        if(!(event.getStatistic().equals(Statistic.JUMP))) {
+            return;
+        }
+
+        player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 20, 1));
     }
 }
