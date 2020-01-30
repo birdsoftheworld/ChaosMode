000ee233b3424dd6003de478a29e5f2e47e22460
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2020-01-29 22:01:36 -0800

    Add MoonGravity

---
+++ src/main/java/birds/chaosMode/ChaosMode/modes/chaosModes/MoonGravity.java
@@ -0,0 +1,13 @@
+package birds.chaosMode.ChaosMode.modes.chaosModes;
+
+import birds.chaosMode.ChaosMode.modes.ListenerMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class MoonGravity extends ListenerMode {
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+
+    }
+}
