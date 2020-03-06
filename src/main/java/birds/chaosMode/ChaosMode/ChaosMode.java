9efc43234352e0f3062d986bd26ef56cde165720
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2020-03-06 13:59:14 -0800

    Disable DeathSwap (for moving to other plugin)

--- src/main/java/birds/chaosMode/ChaosMode/ChaosMode.java
+++ src/main/java/birds/chaosMode/ChaosMode/ChaosMode.java
@@ -46,7 +46,7 @@ public class ChaosMode extends JavaPlugin {
         modes.add(new NeverStop(this));
         modes.add(new DisappearingBlock(this));
         modes.add(new InventoryKerfuffle(this));
-        modes.add(new DeathSwap(this));
+//        modes.add(new DeathSwap(this));
 
         this.getCommand("chaos").setExecutor(new ChaosCommand(this)); // open menu hub
 
